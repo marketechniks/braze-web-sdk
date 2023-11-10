deb5d6015e0eac7decbb9e866ac8a308ca050f9c
marketechniks <145043152+marketechniks@users.noreply.github.com>
2023-11-10 12:14:19 +1100

    Update index.tsx
    Co-Authored-By: marketechniks <145043152+marketechniks@users.noreply.github.com>

--- sample-builds/npm/src/index.tsx
+++ sample-builds/npm/src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+
+import type { ReactElement } from 'react';
+
+declare global {
+  interface Window {
+    Braze: any;
+  }
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
